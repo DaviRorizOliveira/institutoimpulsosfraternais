c1fc70a792d31ee376649abc6eea19f4d3784b57
DaviRorizOliveira <daviro2004@gmail.com>
2024-12-23 16:08:50 -0300

    Add: Seção de objetivos + Fix: Grussura das letras

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,20 +16,23 @@ export default function Home() {
                 <img src={logo.src} alt="Logo" className={styles.logo}/>
             </section>
             
-            <section className={styles.ojectiveSection}>
-                <h3 className={styles.objectiveTitle}>Objetivo</h3>
-                <p className={styles.objectiveTxt}>Nosso foco é levar ajuda às pessoas, buscando estar próximo delas e construir boas memórias através da escuta ativa e do auxílio pontual; conhecer suas necessidades, suas angústias e limitações com o objetivo de promover cidadania no enfrentamento das desigualdades por meio dos direitos socioassistenciais.</p>
-                <div className={styles.info}>
-                    <p className={styles.infoTitle}>O que fazemos: </p>
-                    <p className={styles.infoText}>Alimentamos, Informamos, Orientamos e Educamos</p>
-                </div>
-                <div className={styles.info}>
-                    <p className={styles.infoTitle}>Onde estamos: </p>
-                    <p className={styles.infoText}>No Estado da Bahia, com o sonho de atender a todos Estados Brasileiros</p>
-                </div>
-                <div className={styles.info}>
-                    <p className={styles.infoTitle}>Organizações Sociais Parceiras: </p>
-                    <p className={styles.infoText}>GACC</p>
+            <section className={styles.objectiveSection}>
+                <div className={styles.glassEffect3}></div> {/* Efeito de vidro 3 */}
+                <div className={styles.auxiliar}>
+                    <h3 className={styles.objectiveTitle}>Objetivo</h3>
+                    <p className={styles.objectiveText}>Nosso foco é levar ajuda às pessoas, buscando estar próximo delas e construir boas memórias através da escuta ativa e do auxílio pontual; conhecer suas necessidades, suas angústias e limitações com o objetivo de promover cidadania no enfrentamento das desigualdades por meio dos direitos socioassistenciais.</p>
+                    <div className={styles.info}>
+                        <p className={styles.infoTitle}>O que fazemos:</p>
+                        <p className={styles.infoText}>Alimentamos, Informamos, Orientamos e Educamos</p>
+                    </div>
+                    <div className={styles.info}>
+                        <p className={styles.infoTitle}>Onde estamos:</p>
+                        <p className={styles.infoText}>No Estado da Bahia, com o sonho de atender a todos Estados Brasileiros</p>
+                    </div>
+                    <div className={styles.info}>
+                        <p className={styles.infoTitle}>Organizações Sociais Parceiras:</p>
+                        <p className={styles.infoText}>GACC</p>
+                    </div>
                 </div>
             </section>
             
